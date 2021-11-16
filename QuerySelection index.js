82ada89e1463a8a8d995dd8c7f05ce4754dc0d5b
Noman Hasan <48345904+NomanHasanNM@users.noreply.github.com>
2021-11-16 13:12:22 +0600

    Add files via upload

---
+++ QuerySelection index.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    
+    initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1 id="heading"> This is my new project here is also:-</h1>
+       <p id="para"> I am a nobe dude </p>
+       <a href="#">hOME </a>
+       <ul>
+           <li> 
+               <a href="#"> Tutorial</a>
+               <li> Bangladesh</li>
+        </li>   
+
+          <div class="my-div">
+              <a href="#"> Contact</a>
+          </div>
+           <script src="index.js"></script>
+     
+   
+</body>
+</html>
+
+***********Index.js
+
+
+//getElementById
+var myheading = document.getElementById("heading");
+myheading.innerHTML = "Hello iam a noman hasan shohag babu";
+
+document.getElementById("para").innerHTML = " Bye";
+
+document.querySelector(".my-div a").innerHTML ="I am na ";
