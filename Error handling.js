4dd962f157a435eff7f0cefba48aecec087e1ce6
Noman Hasan <48345904+NomanHasanNM@users.noreply.github.com>
2022-01-12 15:45:54 +0600

    Add files via upload

---
+++ Error handling.js
@@ -0,0 +1,13 @@
+try{
+    alert("Hi I am Noman");
+    alert(x);
+    alert("Bye from Noman Hasan");
+}catch(n)
+{
+    alert("Inside catch block");
+    console.log(n);
+
+}
+finally{
+    alert("Bye from Noman Hasan");
+}
