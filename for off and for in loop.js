d98410dfc721b393032ba82b4c6beb7686a58f36
Noman Hasan <48345904+NomanHasanNM@users.noreply.github.com>
2022-01-15 00:12:36 +0600

    Add files via upload

---
+++ for off and for in loop.js
@@ -0,0 +1,27 @@
+*****for lopp**
+
+const names = ['1 = noman', 'moni', 'mou']
+
+for(let name of names)
+{
+   console.log(name);
+}
+
+
+
+**** for in loop****
+
+
+let students = {
+    ID:182-15-2172,
+    Section: 'PC_C',
+    Batch:50,
+    cgpa: 3.10,
+    projectname: 'Backspaced'
+
+}
+
+for( let x in students)
+{
+    console.log(`${x} : ${students[x]}`);
+}
