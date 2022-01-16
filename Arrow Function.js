3a3827758c543e1353a7bde697e62626b4317556
Noman Hasan <48345904+NomanHasanNM@users.noreply.github.com>
2022-01-16 16:51:49 +0600

    Add files via upload

---
+++ Arrow Function.js
@@ -0,0 +1,27 @@
+function display1() 
+{
+    console.log("I am noman hasan");
+
+}
+display1();
+
+const display2 = () => console.log(" I am a Noman ");
+display2();
+
+function messege1()
+{
+    return " I am Noman Hasan"
+}
+messege1();
+
+const messege2 = () => console.log(" I am also happy with her");
+messege2();
+
+function add1(num1, num2)
+{
+    return num1+num2;
+}
+console.log(add1(30,50));
+
+const add2 = (num1, num2) => num1+num2;
+console.log(add2(40,10))
